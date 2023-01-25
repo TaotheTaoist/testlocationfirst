76bd3c725c18aa6e7e6d347898ae1ef6f6047876
Tao <daowuchen@hotmail.com>
2023-01-25 13:18:37 +0800

    dropped a pin

--- testlocationfirst/ContentView.swift
+++ testlocationfirst/ContentView.swift
@@ -8,14 +8,25 @@ import CoreLocationUI
 import MapKit
 import SwiftUI
 
-struct ContentView: View {
-    
+
+struct ContentView: View{
+//    let customAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054))
     @StateObject var locationManager = LocationManager()
+    let annotations = [
+            City(name: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 37.39149759708518, longitude: -122.08104083062568))
+           
+        ]
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
-                .edgesIgnoringSafeArea(.all)
+
+//            Map(coordinateRegion: $locationManager.region,showsUserLocation: true)
+//                .edgesIgnoringSafeArea(.all)
+            Map(coordinateRegion: $locationManager.region,showsUserLocation: true,annotationItems: annotations)
+            {
+                       MapMarker(coordinate: $0.coordinate)
+                   }
+            .edgesIgnoringSafeArea(.all)
             
             VStack {
                 if let location = locationManager.location {
@@ -26,7 +37,7 @@ struct ContentView: View {
                         .background(.gray)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-
+                
                 Spacer()
                 HStack {
                     LocationButton {
@@ -35,57 +46,47 @@ struct ContentView: View {
                     .frame(width: 180, height: 40)
                     .cornerRadius(30)
                     .symbolVariant(.fill)
-                .foregroundColor(.white)
+                    .foregroundColor(.white)
+                    Button(action: {
+                        let newLocation = CLLocationCoordinate2D(latitude: 40.730610, longitude: -73.935242)
+                        self.locationManager.region = MKCoordinateRegion(center: newLocation, span: MapDetails.defaultSpan)
+                    })
+                    {
+                        Text("New York City")
+                    }
+                    
                 }
-                Button(action: {
-                                    let newLocation = CLLocationCoordinate2D(latitude: 40.730610, longitude: -73.935242)
-                                    self.locationManager.region = MKCoordinateRegion(center: newLocation, span: MapDetails.defaultSpan)
-                                })
-                {
-                                    Text("New York City")
-                                }
-
-
             }
             .padding()
         }
     }
 }
 
-//struct ContentView: View {
-//    @StateObject private var viewModel = ContentViewModel()
+//class CustomAnnotation: NSObject, MKAnnotation,Identifiable {
+//    var coordinate: CLLocationCoordinate2D
 //
-//    var body: some View {
-//        //shows user location IF we had permissions
-//        ZStack (alignment: .bottom){
-//            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
-//                .ignoresSafeArea()
-//                .accentColor(Color(.systemPink))
-//                .onAppear{
-//                    viewModel.checkIfLocationServicesIsEnabled()
-//
-//                }
-//            HStack {
-//                LocationButton(.currentLocation){
-//                    viewModel.requestUserLocationForOnce()
-//                }
-//                .foregroundColor(.white)
-//                .cornerRadius(8)
-//                .padding()
-//                Button(action: {
-//                    let newLocation = CLLocationCoordinate2D(latitude: 40.730610, longitude: -73.935242)
-//                    self.viewModel.region = MKCoordinateRegion(center: newLocation, span: MapDetails.defaultSpan)
-//                }) {
-//                    Text("New York City")
-//                }
-//                    .cornerRadius(8)
-//                    .padding()
-//            }
-//        }
+//    init(coordinate: CLLocationCoordinate2D) {
+//        self.coordinate = coordinate
 //    }
 //}
-
-
+//struct MapView: UIViewRepresentable {
+//    var annotations: [MKAnnotation]
+//
+//    func makeUIView(context: Context) -> MKMapView {
+//        let mapView = MKMapView()
+//        mapView.addAnnotations(annotations)
+//        return mapView
+//    }
+//
+//    func updateUIView(_ uiView: MKMapView, context: Context) {
+//        uiView.addAnnotations(annotations)
+//    }
+//}
+struct City: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- testlocationfirst/ContentViewModel.swift
+++ testlocationfirst/ContentViewModel.swift
@@ -4,9 +4,10 @@
 //
 //  Created by 2015 MBP 16GB 256GB on 1/23/23.
 //
-import CoreLocation
 import Foundation
+import CoreLocation
 import MapKit
+
 final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     
@@ -46,87 +47,5 @@ enum MapDetails {
     static let startingLocation = CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054)
     static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
 }
-//
-//
-//final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-//    @Published var location: CLLocationCoordinate2D?
-//
-//    @Published var region = MKCoordinateRegion(
-//        center: MapDetails.startingLocation,
-//        span: MapDetails.defaultSpan
-//    )
-//    var locationManager: CLLocationManager?
-//
-//    override init() {
-//        super.init()
-//        locationManager?.delegate = self
-//
-//    }
-//    func requestUserLocationForOnce() {
-//        locationManager!.requestLocation()
-//        }
-//
-//
-//    internal func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-//           checkLocationAuthorization()
-//           locationManager?.startUpdatingHeading()
-//       }
-//
-//    func checkIfLocationServicesIsEnabled() {
-//        DispatchQueue.main.async {
-//            if CLLocationManager.locationServicesEnabled() {
-//                self.locationManager = CLLocationManager()
-//                self.locationManager!.delegate = self
-//            } else {
-//                print("Show an alert letting them know this is off and to go turn it on.")
-//            }
-//        }
-//    }
-//
-//
-//    private func checkLocationAuthorization() {
-//
-//        DispatchQueue.main.async {
-//            guard let locationManager = self.locationManager else {return}
-//
-//            switch locationManager.authorizationStatus {
-//
-//            case .notDetermined:
-//                locationManager.requestWhenInUseAuthorization()
-//            case .restricted:
-//                print("Your location is restricted likely due to parental controls")
-//            case .denied:
-//                print("You have denied this app location permission. Go into settings to change it.")
-//            case .authorizedAlways, .authorizedWhenInUse:
-//                self.region = MKCoordinateRegion(
-//                    center: locationManager.location!.coordinate ,
-//                    span: MapDetails.defaultSpan
-//                )
-//            @unknown default:
-//                break
-//            }
-//        }
-//    }
-//
-////    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-////            guard let latestLocation = locations.first else {return}
-////
-////            self.region = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-////        }
-////
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//            guard let location = locations.first else { return }
-//
-//            DispatchQueue.main.async {
-//                self.location = location.coordinate
-//                self.region = MKCoordinateRegion(
-//                    center: location.coordinate,
-//                    span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-//                )
-//            }
-//        }
-//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-//            print(error.localizedDescription)
-//        }
-//
-//}
+
+
